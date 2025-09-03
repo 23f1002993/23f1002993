ec09bd65e5de555327f72c92cbe6e10dcfb4cfab
23f1002993 <23f1002993@ds.study.iitm.ac.in>
2025-09-03 20:35:47 +0530

    solved-minor-issue

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from datetime import timedelta
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-vehicle-parking-2025'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///quiz_master.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=1)
     REMEMBER_COOKIE_DURATION = timedelta(days=14)
